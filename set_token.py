e26aa59c9d03699ae0545936d96f7d8b3cddbdf3
Tahseen Jamal <tahseen.jamal@gmail.com>
2023-04-16 23:27:35 +0530

    placed variables before use for ease of understanding

--- set_token.py
+++ set_token.py
@@ -3,6 +3,9 @@ import redis
 
 # Write the tokens in Redis so that other applications can use them
 
+access_token = 'YOUR ACCESS TOKEN'
+public_token = 'YOUR PUBLIC TOKEN'
+
 redis_client = redis.Redis()
 redis_client.hset("token.{}".format(userid), "access_token", access_token)
 redis_client.hset("token.{}".format(userid), "public_token", public_token)
